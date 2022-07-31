33c87849756040e2090f7897be8679b378ba455b
Happy Singh <hsingh0805@gmail.com>
2022-07-31 16:14:49 +0530

    Add Functional and class Component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import {Student, Teacher} from './Student';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Student name = "Happy"/>, document.getElementById("root"));   // render(elment, donMode)
+ReactDOM.render(<Teacher name = "Youtube launda"/>, document.getElementById("root"));
