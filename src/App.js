f5964068eb3843592e60497eeed0daa0236870f7
Happy Singh <hsingh0805@gmail.com>
2022-07-31 16:19:47 +0530

    Composing components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component, Fragment} from "react";
-
+import { Teacher, Student } from "./Student";
 class App extends Component {
     render() {
         // return React.createElement("h1", null, "Hello Happy"); // Normal code  (type, props, children)
         return <React.Fragment>
-            <h1>Hello Happy</h1>
-            <h1>Hello Ghost</h1>
+            <Teacher name="Teacher"/>
+            <Student name="Student"/>
             </React.Fragment>
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import {Student, Teacher} from './Student';
 
-ReactDOM.render(<Student name = "Happy"/>, document.getElementById("root"));   // render(elment, donMode)
-ReactDOM.render(<Teacher name = "Youtube launda"/>, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
