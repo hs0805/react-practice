8bef65773c6011f0d089df91e57905f648ca237d
Happy Singh <hsingh0805@gmail.com>
2022-07-31 18:24:44 +0530

    Adding class attribute to element in JSX

--- src/Student.js
+++ src/Student.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import "./App.css"
 
 const Student = (props) => {
-    return <h1>Hello {props.name}</h1>
+    return <h1 className="bg">Hello {props.name}</h1>
 }
 
 class Teacher extends Component {
     render() {
-        return <h1> Hello {this.props.name}</h1>
+        return <h1 className="bg"> Hello {this.props.name}</h1>
     }
 }
 
