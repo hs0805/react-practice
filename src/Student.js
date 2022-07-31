a0151506deb77c2468752e760268c95962f155f5
Happy Singh <hsingh0805@gmail.com>
2022-07-31 19:22:40 +0530

    Events in react

--- src/Student.js
+++ src/Student.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react'
 
 class Student extends Component {
-    state = {
-        name: "Happy",
-        roll: this.props.roll
+    
+    // binding of this with function
+    constructor( props ) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
     }
+
+    handleClick()  {
+        console.log("Button is clicked", this);
+    }
+
+    handleRedirect = (e) => {
+        e.preventDefault();
+        console.log("Link is clicked", this);
+    }
+    // OR
+    // USE ARROW FUNCTION
+
+
+    // handleClick = () => {
+    //     console.log("Button is clicked", this);
+    // }
 
     render() {
         return (
-            <h1> Hello {this.state.name} and roll no. is : {this.state.roll} </h1>
+            <div>
+                <h1> Hello Event </h1>
+                <button onClick= {this.handleClick}> Click Me </button>
+                <a href='https://github.com/hs0805/react-practice' onClick={ this.handleRedirect }> Github repo </a>
+            </div>
         )
     }
 }
