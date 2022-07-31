6d1880fc88411dd0b0e33654005eb7c499edbf11
Happy Singh <hsingh0805@gmail.com>
2022-07-31 19:31:44 +0530

    setState method in react

--- src/Student.js
+++ src/Student.js
@@ -2,34 +2,26 @@ import React, { Component } from 'react'
 
 class Student extends Component {
     
-    // binding of this with function
-    constructor( props ) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
+    state = {
+        name: "Happy",
+        roll: this.props.roll
     }
 
-    handleClick()  {
-        console.log("Button is clicked", this);
+    handleClick = () => {
+        this.setState({ name: "Ghost", roll: "1"});
+        this.setState( (state, props) => {
+            console.log(state.name);
+            console.log(state.roll);
+            console.log(props);
+        })
     }
-
-    handleRedirect = (e) => {
-        e.preventDefault();
-        console.log("Link is clicked", this);
-    }
-    // OR
-    // USE ARROW FUNCTION
-
-
-    // handleClick = () => {
-    //     console.log("Button is clicked", this);
-    // }
 
     render() {
         return (
             <div>
-                <h1> Hello Event </h1>
+                <h1> Name is : {this.state.name} </h1>
+                <h1> roll number is : {this.state.roll} </h1>
                 <button onClick= {this.handleClick}> Click Me </button>
-                <a href='https://github.com/hs0805/react-practice' onClick={ this.handleRedirect }> Github repo </a>
             </div>
         )
     }
