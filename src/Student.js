4d2c3d152e1bc4bc7ab6433cf98fcadd15db263c
Happy Singh <hsingh0805@gmail.com>
2022-07-31 20:04:47 +0530

    Passing arguments to event handlers

--- src/Student.js
+++ src/Student.js
@@ -7,13 +7,23 @@ class Student extends Component {
         roll: this.props.roll
     }
 
-    handleClick = () => {
+    handleClick = (arg, e) => {
         this.setState({ name: "Ghost", roll: "1"});
         this.setState( (state, props) => {
             console.log(state.name);
             console.log(state.roll);
             console.log(props);
+            console.log("Event is :", e);
         })
+    }
+
+    handleClickArg = (e) => {
+        console.log("Clicked through argumented method");
+        this.handleClick(this.handleClick(this.state.id, e));
+    }
+
+    automaticallyCalled = (id) => {
+        console.log("Function called auomatically :", id);
     }
 
     render() {
@@ -21,7 +31,28 @@ class Student extends Component {
             <div>
                 <h1> Name is : {this.state.name} </h1>
                 <h1> roll number is : {this.state.roll} </h1>
-                <button onClick= {this.handleClick}> Click Me </button>
+                {/* bind method */}
+                <button onClick= { 
+                    this.handleClick.bind(this, this.state.id)
+                }
+                > 
+                binded pass
+                 </button>
+
+                 {/* using argumented method */}
+                 <button onClick={this.handleClickArg}>
+                    argumented pass
+                 </button>
+
+                 {/* using anonumous pass */}
+                 <button onClick={
+                    e => {
+                        this.handleClick(this.state.roll, e);
+                    }
+                 }>
+                    anonumous
+                 </button>
+                <button onClick= {this.automaticallyCalled("101")}> Automatic call </button>
             </div>
         )
     }
