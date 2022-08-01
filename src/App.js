bab4ff97347d0063b37620840ba3c447a7ee7698
Happy Singh <hsingh0805@gmail.com>
2022-08-01 11:00:11 +0530

    Lists in react

--- src/App.js
+++ src/App.js
@@ -3,27 +3,31 @@ import User from './User'
 import Guest from "./Guest";
 
 class App extends Component {
-    state = {
-        isLoggedIn: true
-    }
 
-    clickLogin = () => {
-        this.setState({isLoggedIn: true})
-    }
+    render() {
 
-    clickLogout = () => {
-        this.setState({isLoggedIn: false})
-    }
+        const arr = [10, 20, 30, 40, 50, 60];
+        const newArr = arr.map((num) => {
+            return <li> {num*2} </li>
+        })
 
-    render(){
-        return(
-            <React.Fragment>
-                {(this.state.isLoggedIn && <User /> ) || (<Guest/>) }
-                <button onClick={this.clickLogin}> Login </button>
-                <button onClick={this.clickLogout}> Logout </button>
-            </React.Fragment>
+        return (
+            <div>
+                <ul>{newArr}</ul>
+                <br></br>
+                <ul>
+                    {
+                        arr.map((num) => {
+                           return <li>{num}</li>
+                        })
+                    }
+                </ul>
+            </div>
         )
+    
     }
+
+
 }
 
 export default App;
